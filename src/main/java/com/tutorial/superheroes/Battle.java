5d2701a51cbaa248b17ab8be4bab6f65aad1abd0
Batman <batman.dev@email.com>
2020-06-07 07:27:55 +0200

    Añadida clase Battle

---
+++ src/main/java/com/tutorial/superheroes/Battle.java
@@ -0,0 +1,31 @@
+package com.tutorial.superheroes;
+
+/**
+ * Simula una batalla entre dos héroes
+ * @author david
+ */
+public class Battle {
+	
+	private Hero hero1;
+	private Hero hero2;
+	
+	/**
+	 * Constructor
+	 * @param hero1
+	 * @param hero2
+	 */
+	public Battle(Hero hero1, Hero hero2) {
+		super();
+		this.hero1 = hero1;
+		this.hero2 = hero2;
+	}
+
+	/**
+	 * Devuelve el ganador de la batalla
+	 * @return Ganador
+	 */
+	public Hero getWinner() {
+		return this.hero1;
+	}
+
+}
